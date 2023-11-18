0c68af6360c0be8fb44a75b09dec601b01e2406a
Ritesh Kumar <singlaritesh007@gmail.com>
2023-11-18 17:51:37 +0530

    moved queryinput to main page

--- src/rbac/constants/routes.js
+++ src/rbac/constants/routes.js
@@ -1,15 +1,9 @@
-import Home from '../../pages/Home/Home';
 import QueryInput from '../../pages/QueryInput/QueryInput';
 import QueryResult from '../../pages/QueryResults/QueryResults';
 
 const ROUTES = [
   {
     link: '/',
-    name: 'Admin home',
-    component: <Home />
-  },
-  {
-    link: '/input',
     name: 'Query input',
     component: <QueryInput />
   },
